57eb56d124a75a5cedcdc8f61a073de1e81434d3
Chandrakala Kamparaju <chandrakalakamparaju97@gmail.com>
2026-08-07 18:52:55 +0530

    Added Day 4 - Input & Output in Python

---
+++ day04_input & output.py
@@ -0,0 +1,88 @@
+# =====================================================================================
+# DAY 4 - INPUT AND OUTPUT IN PYTHON
+# =====================================================================================
+# ==========Program 1: Simple Output==========
+print("Welcome to Python Programmming")
+print("My name is Chandrakala")
+print("I'm learning Input & Output in Python")
+print("\n")
+
+# OUTPUT:
+# Welcome to Python Programmming
+# My name is Chandrakala
+# I'm learning Input & Output in Python
+
+# ==========Program 2: Taking Name as Input==========
+name = input("Enter your name:")
+
+print("Hello", name)
+print("\n")
+
+# OUTPUT:
+# Enter your name:chandu
+# Hello chandu
+
+# ==========Program 4: Integer Input==========
+age = int(input("Enter your age:"))
+
+print("Your age is:", age)
+print(type(age))
+print("\n")
+
+# OUTPUT:
+# Enter your age:20
+# Your age is: 20
+# <class 'int'>
+
+# ==========Program 5: Float Input==========
+percentage = float(input("Enter your percentage:"))
+
+print("Your percentage:", percentage)
+print(type(percentage))
+print("\n")
+
+# OUTPUT:
+# Enter your percentage:87.7
+# Your percentage: 87.7
+# <class 'float'>
+
+# ==========Program 6: Multiple Inputs==========
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+
+print("Name :", name)
+print("City :", city)
+print("\n")
+
+# OUTPUT:
+# Enter your name: chandu
+# Enter your city: Tadipatri
+# Name : chandu
+# City : Tadipatri
+
+# ==========Program 7: Input Two Numbers==========
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("First Number :", num1)
+print("Second Number :", num2)
+print("\n")
+
+# OUTPUT:
+# Enter first number: 7
+# Enter second number: 9
+# First Number : 7
+# Second Number : 9
+
+# ==========Program 10: Personal Information==========
+Name = input("Enter your name:")
+Roll_no = int(input("Enter your Roll no:"))
+Per = float(input("Enter your percentage:  "))
+Company = input("Enter your dream company:")
+
+print("Name = ", Name)
+print("Roll No = ", Roll_no)
+print("Percentage = ", Per)
+print("Company = ", Company)
+
+# OUTPUT:
