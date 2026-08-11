f9e1ddb7e1c5386722d2cb7ad21803cea477ed37
Chandrakala Kamparaju <chandrakalakamparaju97@gmail.com>
2026-08-11 20:07:55 +0530

    Added Day 6 - Type Conversion in Python

---
+++ day06_type coversion.py
@@ -0,0 +1,72 @@
+# =====================================================================================
+# DAY 6 - TYPE CONVERSION IN PYTHON
+# =====================================================================================
+# ==========Program 1: String to Integer ==========
+age = "20"
+age = int(age)
+print(age)
+print(type(age))
+print("\n")
+
+# OUTPUT:
+# 20
+# <class 'int'>
+
+# ==========Program 2: Integer to Float==========
+number = 10
+number = float(number)
+print(number)
+print(type(number))
+print("\n")
+
+# OUTPUT:
+# 10.0
+# <class 'float'>
+
+# ==========Program 3: Integer to String==========
+marks = 95
+marks = str(marks)
+print(marks)
+print(type(marks))
+print("\n")
+
+# OUTPUT:
+# 95
+# <class 'str'>
+
+# ==========Program 4: String to Float==========
+price = "99.50"
+price = float(price)
+print(price)
+print(type(price))
+print("\n")
+
+# OUTPUT:
+# 99.5
+# <class 'float'>
+
+# ==========Program 5: Integer to Boolean==========
+x = 1
+print(bool(x))
+y = 0
+print(bool(y))
+print("\n")
+
+# OUTPUT:
+# True
+# False
+
+# ==========Program 6: Type Conversion with Input==========
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+total = first_number + second_number
+print("Total:", total)
+
+# OUTPUT:
+# Enter first number: 9
+# Enter second number: 7
+# Total: 16
+
+# =====================================================================================
+# END OF DAY 6
+# =====================================================================================
